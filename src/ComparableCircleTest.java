b0d8426081149a6eb42aa802c673d75a616ca59b
Hung <mqhung93@gmail.com>
2020-12-17 11:03:32 +0700

    thuc hanh interface co san trong java

---
+++ src/ComparableCircleTest.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ComparableCircleTest {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(31.8);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle( "indigo", false, 28.4);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
